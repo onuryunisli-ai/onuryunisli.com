43d78d5e8d8c3878c576724798dc1e7b102fea6e
Onur Yunisli <onuryunisli@gmail.com>
2026-09-19 18:21:45 +0400

    sdsd

--- data/content.js
+++ data/content.js
@@ -7,7 +7,7 @@ window.SITE = {
   "city": "Baku",
   "country": "Azerbaijan",
   "timezone": "Asia/Baku",
-  "portrait": "https://res.cloudinary.com/dss5nccfs/image/upload/v1789802987/onuryunisli/hdszlgma8jgvdgwzqx7k.png",
+  "portrait": "/assets/brand/portrait.jpg",
   "social": {
     "behance": "https://www.behance.net/onuryunisli",
     "instagram": "https://www.instagram.com/onuryunisli/",
